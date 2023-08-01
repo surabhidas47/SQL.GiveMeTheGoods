7f61de1613930e0b54de7e958b28ac6dec541865
surabhi <surabhi.das@outlook.com>
2023-08-01 17:29:57 -0400

    did all exercises

---
+++ answers/exercise2.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM Students
+WHERE City LIKE 'a%'
+OR City LIKE 'c%'
+OR City LIKE 's%';
---
+++ answers/exercise4.sql
@@ -0,0 +1,5 @@
+SELECT *
+FROM Students
+WHERE City NOT LIKE 'a%'
+OR City NOT LIKE 'c%'
+OR City NOT LIKE 'f%';
